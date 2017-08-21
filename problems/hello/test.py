6b344a274ae44669c05a164cd9542b53dc49cc3e
Ken Youens-Clark <kyclark@gmail.com>
2017-08-21 14:05:22 -0700

    did a pylint test.py

--- problems/hello/test.py
+++ problems/hello/test.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python3
+"""test the hellos"""
 
 from subprocess import getstatusoutput, getoutput
 import os.path
 import re
 
-scripts = ["hello.sh", "hello-arg.sh", "hello-arg2.sh"]
 
 def test_exists():
-    for script in scripts:
+    """test that scripts exist"""
+    for script in ["hello.sh", "hello-arg.sh", "hello-arg2.sh"]:
         assert os.path.exists(script)
 
 def test_hello_out():
-    out = getoutput("bash hello.sh") 
+    """test"""
+    out = getoutput("bash hello.sh")
     assert out == "Hello, World!"
 
 def test_hello_arg_usage():
-    (retval, out) = getstatusoutput("bash hello-arg.sh") 
+    """test"""
+    (retval, out) = getstatusoutput("bash hello-arg.sh")
     assert retval > 0
     assert re.match("usage", out, re.IGNORECASE)
 
 def test_hello_arg():
-    out1 = getoutput("bash hello-arg.sh Doctor") 
+    """test"""
+    out1 = getoutput("bash hello-arg.sh Doctor")
     assert out1 == "Hello, Doctor!"
 
-    out2 = getoutput("bash hello-arg.sh Bowzer") 
+    out2 = getoutput("bash hello-arg.sh Bowzer")
     assert out2 == "Hello, Bowzer!"
 
 def test_hello_arg2_usage():
-    (retval, out) = getstatusoutput("bash hello-arg2.sh") 
+    """test"""
+    (retval, out) = getstatusoutput("bash hello-arg2.sh")
     assert retval > 0
     assert re.match("usage", out, re.IGNORECASE)
 
-def test_hello_arg():
-    out1 = getoutput("bash hello-arg2.sh Bonjour Madam") 
+def test_hello_arg2():
+    """test"""
+    out1 = getoutput("bash hello-arg2.sh Bonjour Madam")
     assert out1 == "Bonjour, Madam!"
 
-    out2 = getoutput("bash hello-arg2.sh 'Good Boy' Bowzer") 
+    out2 = getoutput("bash hello-arg2.sh 'Good Boy' Bowzer")
     assert out2 == "Good Boy, Bowzer!"
