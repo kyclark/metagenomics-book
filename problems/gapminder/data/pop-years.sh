3c9fd5ebe1717b39fca5b787b8467370e710e807
Ken Youens-Clark <kyclark@gmail.com>
2017-08-29 16:03:06 -0700

    nuther version

---
+++ problems/gapminder/data/pop-years.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -u
+
+YEARS="years"
+
+cut -f 2 ./*.cc.txt | sort | uniq > "$YEARS"
+NUM=$(wc -l $YEARS | awk '{print $1}')
+
+if [[ "$NUM" -lt 1 ]]; then
+  echo "No years ($NUM)!"
+  exit 1
+fi
+
+while read -r YEAR; do 
+    echo -n "$YEAR: " 
+    awk -F"\t" "\$2 == $YEAR { printf \"%d\\n\", \$3 }" ./*.cc.txt | grep -v "[a-z]" | paste -sd+ - | bc
+done < "$YEARS"
+
+rm "$YEARS"
