871b9ca4c545c38da62df24a82034062980fd3fb
Ken Youens-Clark <kyclark@gmail.com>
2017-08-29 16:14:32 -0700

    foo

--- problems/gapminder/data/pop-years.sh
+++ problems/gapminder/data/pop-years.sh
@@ -16,5 +16,3 @@ while read -r YEAR; do
     echo -n "$YEAR: " 
     awk -F"\t" "\$2 == $YEAR { printf \"%d\\n\", \$3 }" ./*.cc.txt | grep -v "[a-z]" | paste -sd+ - | bc
 done < "$YEARS"
-
-rm "$YEARS"
