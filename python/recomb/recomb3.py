49246f2e0dbf8f4d35ac65f6e39768280961aa32
Ken Youens-Clark <kyclark@gmail.com>
2017-10-03 14:56:50 -0700

    other versions using list comprehension, functions

---
+++ python/recomb/recomb3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Show recominations"""
+
+import os
+import sys
+from itertools import product, chain
+
+args = sys.argv
+
+if len(args) != 2:
+    print('Usage: {} NUM_GENES'.format(os.path.basename(args[0])))
+    sys.exit(1)
+
+if not args[1].isdigit():
+    print('"{}" does not look like an integer'.format(args[1]))
+    sys.exit(1)
+
+num_genes = int(args[1])
+if not 2 <= num_genes <= 10:
+    print('NUM_GENES must be greater than 1, less than 10')
+    sys.exit(1)
+
+def gen(prefix):
+    return [prefix + str(n + 1) for n in range(0, num_genes)]
+
+promotors = gen('P')
+coding = gen('C')
+terminators = gen('T')
+
+print('N = "{}"'.format(num_genes))
+for i, combo in enumerate(chain(product(promotors, coding, terminators))):
+    print('{:3}: {}'.format(i + 1, combo))
