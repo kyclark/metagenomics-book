9d489e4894947e98b950c6302ead24c4e6d109ce
Ken Youens-Clark <kyclark@gmail.com>
2017-10-12 11:14:06 -0700

    my answers

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 """say hello to the arguments"""
 
-print('OK')
+import sys
+import os
+
+args = sys.argv
+
+if len(args) < 2:
+    print('Usage: {} NAME [NAME2 ...]'.format(os.path.basename(args[0])))
+    sys.exit(1)
+
+names = args[1:]
+num = len(names)
+phrase = ''
+if num == 1:
+    phrase = names[0]
+elif num == 2:
+    phrase = '{} and {}'.format(names[0], names[1])
+else:
+    last = names.pop()
+    phrase = '{}, and {}'.format(', '.join(names), last)
+
+print('Hello to the {} of you: {}!'.format(num, phrase))
--- problems/py-strings/gc.py
+++ problems/py-strings/gc.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print('OK')
+import os
+import sys
+
+args = sys.argv[1:]
+
+if len(args) != 1:
+    print('Usage: {} ARG'.format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
+
+dna = args[0]
+
+print('Arg is "{}"'.format(arg))
