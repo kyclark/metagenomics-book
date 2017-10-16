4e5f1639e784be707df8e0edf1a0e5dc2c357dff
Ken Youens-Clark <kyclark@gmail.com>
2017-10-16 17:22:51 -0500

    check first x records in delimited file

---
+++ python/csv/tabchk.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import os
+import sys
+
+# --------------------------------------------------
+def get_args():
+    parser = argparse.ArgumentParser(description='Check a delimited text file')
+    parser.add_argument('file', metavar='str', help='File')
+    parser.add_argument('-s', '--sep', help='Field separator',
+                        metavar='str', type=str, default='\t')
+    parser.add_argument('-l', '--limit', help='How many records to show',
+                        metavar='int', type=int, default=1)
+    parser.add_argument('-d', '--dense', help='Not sparse (skip empty fields)',
+                        action='store_true')
+    return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+    args = get_args()
+    file = args.file
+    limit = args.limit
+    sep = args.sep
+    dense = args.dense
+
+    if not os.path.isfile(file):
+        print('"{}" is not a file'.format(file))
+        sys.exit(1)
+
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=sep)
+
+        for i, row in enumerate(reader):
+            vals = dict([x for x in row.items() if x[1] != '']) if dense else row
+            flds = vals.keys()
+            longest = max(map(len, flds))
+            fmt = '{:' + str(longest + 1) + '}: {}'
+            print('// ****** Record {} ****** //'.format(i+1))
+            for key, val in vals.items():
+                print(fmt.format(key, val))
+
+            if i + 1 == limit:
+                break
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
