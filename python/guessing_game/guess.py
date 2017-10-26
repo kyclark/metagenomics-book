aa8fdf678e4db3c68d3c46a72da92b2dafa3a61f
Ken Youens-Clark <kyclark@gmail.com>
2017-10-25 20:34:02 -0700

    new version

--- python/guessing_game/guess.py
+++ python/guessing_game/guess.py
@@ -1,24 +1,56 @@
 #!/usr/bin/env python3
+"""guess the number game"""
 
+import argparse
 import random
+import sys
 
-secret = random.randint(1,101)
-print("The secret number is %s" % secret)
+# --------------------------------------------------
+def get_args():
+    """get args"""
+    parser = argparse.ArgumentParser(description='Argparse Python script')
+    parser.add_argument('-m', '--min', help='Minimum value',
+                        metavar='int', type=int, default=1)
+    parser.add_argument('-x', '--max', help='Maximum value',
+                        metavar='int', type=int, default=50)
+    parser.add_argument('-g', '--guesses', help='Number of guesses',
+                        metavar='int', type=int, default=5)
+    return parser.parse_args()
 
-while True:
+# --------------------------------------------------
+def main():
+    """main"""
+    args = get_args()
+    low = args.min
+    high = args.max
+    guesses_allowed = args.guesses
+
+    secret = random.randint(low, high)
+    num_guesses = 0
+    prompt = 'Guess a number between {} and {}: '.format(low, high)
+
     while True:
-        try:
-            guess = int(input("Please enter a number: "))
+        guess = input('[{}] {}'.format(num_guesses, prompt))
+        num_guesses += 1
+
+        if not guess.isdigit():
+            print('"{}" is not a number'.format(guess))
+            continue
+
+        print('You guessed "{}"'.format(guess))
+        num = int(guess)
+
+        if num_guesses >= guesses_allowed:
+            print('Too many guesses! The number was "{}."'.format(secret))
+            sys.exit()
+        elif num == secret:
+            print('You win!')
             break
-        except ValueError:
-            print("That was not a number")
+        elif num < secret:
+            print('Too low.')
+        else:
+            print('Too high.')
 
-    print("You guessed %s" % guess)
-
-    if guess == secret:
-        print("You win!")
-        break
-    elif guess < secret:
-        print("Too low.")
-    else:
-        print("Too high.")
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
