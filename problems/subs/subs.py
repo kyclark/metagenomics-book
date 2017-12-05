d59f5ecbc3891a671189495cb9e263ca65461afb
Ken Youens-Clark <kyclark@gmail.com>
2017-12-05 11:11:35 -0700

    fixed tests

--- problems/subs/subs.py
+++ problems/subs/subs.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
-print('OK')
+import os
+import sys
+#from collections import defaultdict
+
+args=sys.argv[1:]
+if len(args) < 2:
+    print("Usage: requires two strings")
+    sys.exit(1)
+
+s=args[0]
+t=args[1]
+counts=[]
+
+for letter in range(len(s)):
+    if s[letter:letter+len(t)] == t: #look at each t length segment
+        counts.append(letter+1) #add 1 to account for offset
+
+if counts:
+    print(*counts, sep=' ')
+else:
+    print('Not found')
